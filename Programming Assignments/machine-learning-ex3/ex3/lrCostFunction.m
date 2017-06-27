1c308216ff3b6096ef06dbb17134d2b924d34161
NegroKetchum <negro.ketchum.github@gmail.com>
2017-06-27 15:49:44 -0700

    Finish programming assignment 3.

--- Programming Assignments/machine-learning-ex3/ex3/lrCostFunction.m
+++ Programming Assignments/machine-learning-ex3/ex3/lrCostFunction.m
@@ -36,9 +36,9 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+J = (1/m)*(-y'*log(sigmoid(X*theta))-(1-y)'*log(1-sigmoid(X*theta))) + (lambda/(2*m))*sum(theta(2:end).^2);
 
-
-
+grad = (1/m)*X'*(sigmoid(X*theta)-y) + [0;(lambda/m).*theta(2:end)];
 
 
 
